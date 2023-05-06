be1124ca600c97fae43a706eb2ee6eb9c21e00c7
h04x <liquid.doctrine@gmail.com>
2023-05-06 23:28:20 +0300

    liecense

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,9 +2,8 @@ use dht_get_peers::get_peers;
 
 fn main() {
     // debian-11.6.0
-    let info_hash =
-        b"\x6d\x47\x95\xde\xe7\x0a\xeb\x88\xe0\x3e\x53\x36\xca\x7c\x9f\xcf\x0a\x1e\x20\x6d";
+    let info_hash = b"\x6d\x47\x95\xde\xe7\x0a\xeb\x88\xe0\x3e\
+         \x53\x36\xca\x7c\x9f\xcf\x0a\x1e\x20\x6d";
 
-    let r = get_peers(*info_hash);
-    dbg!(r);
+    dbg!(get_peers(*info_hash));
 }
